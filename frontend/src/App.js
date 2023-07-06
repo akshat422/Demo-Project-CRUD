4ead39d21b25f723e256b32d235e965bb854cb38
Akshat Kotwalla <akshatkotwalla@gmail.com>
2023-07-06 10:33:54 +0530

    Router Working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,32 @@ import './App.css';
 import Profile from './components/ProfilePage';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Routes,
+} from "react-router-dom";
 import UploadWork from './components/UploadWork';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-      <Navbar/>
-        <Switch>
-            <Route exact path="/" component={Profile}/>
-            <Route path="/reportform" component={UploadWork} />
-        </Switch>
-      </Router>
+      {/* <Router>
+        <Navbar/>
+          <Routes>
+            <Route exact path="/" element={Profile}/>
+            <Route path="/reportform" element={UploadWork} />
+          </Routes>
+      </Router> */}
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Profile />} />
+        <Route path="/reportform" element={<UploadWork />} />
+      </Routes>
+    </BrowserRouter>
+
     </div>
 
   );
